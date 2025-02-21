ac4dbb4897ea75a9c776d20829d9dd0d830e8db0
dkdla93 <67941919+dkdla93@users.noreply.github.com>
2025-02-21 11:39:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-# main.py
 import streamlit as st
-import report_revenue
-import report_settlement
-import search_shorts
-import search_meme
-import search_creator
 
-st.title("East Blue 업무지원 서비스")
+def main():
+    st.title("East Blue 업무지원 서비스")
+    st.write("환영합니다! 여기는 홈 화면입니다.")
 
-page = st.sidebar.selectbox("서비스 선택", ["홈", "음원정산시트 생성기", "정산보고서 생성기", "쇼츠 수집 및 다운로드", "밈 검색기", "크리에이터 검색기"])
-
-if page == "홈":
-    st.write("환영합니다!")
-elif page == "음원정산시트 생성기":
-    report_revenue.main()  # 파일 내 정의된 main() 함수 호출
-elif page == "정산보고서 생성기":
-    report_settlement.main()
-elif page == "쇼츠 수집 및 다운로드":
-    search_shorts.main()
-elif page == "밈 검색기":
-    search_meme.main()
-elif page == "크리에이터 검색기":
-    search_creator.main()
+if __name__ == "__main__":
+    main()
